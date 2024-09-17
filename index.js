74de400e3818ad5685a836924e4bcc2de2333826
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2024-09-17 23:29:46 +0300

    first

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let tg = window.Telegram.WebApp; //нужно получить объект window.Telegram.WebApp Телеграмма
 // tg.themeParams.bottom_bar_bg_color = "#1468B1";
 //tg.backgroundColor = "#EFEFF4";
-tg.headerColor = "#1468B1";
+// tg.headerColor = "#1468B1";
 // tg.bottomBarColor = "#1468B1";
 // tg.backgroudColor = "#efeff4";
 // tg.colorScheme = "light";
