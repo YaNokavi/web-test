fb2b915762091a864f6626ec835ebfbe4a94890a
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2024-09-19 00:19:04 +0300

    light

---
+++ setcolor.js
@@ -0,0 +1,7 @@
+(function() {
+    // Проверьте, доступен ли объект Telegram WebApp
+    if (window.Telegram.WebApp) {
+        // Установите цвет заголовка
+        window.Telegram.WebApp.setHeaderColor('bg_color', '#1468B1'); // Замените на ваш цвет
+    }
+})();
