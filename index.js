5647aea867d544b229f53720aa398e47ea8e23f7
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2024-09-20 16:45:12 +0300

    course

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-"use strict";
+// "use strict";
 
-let tg = window.Telegram.WebApp;
+// let tg = window.Telegram.WebApp;
 
-tg.setHeaderColor("#1468B1");
-tg.expand();
+// tg.setHeaderColor("#1468B1");
+// tg.expand();
 
-var userIdData = "User ID: " + `${tg.initDataUnsafe.user.id}`;
-var logoname = `${tg.initDataUnsafe.user.username}`[0] + `${tg.initDataUnsafe.user.username}`[1];
-var username = `${tg.initDataUnsafe.user.username}`;
+// var userIdData = "User ID: " + `${tg.initDataUnsafe.user.id}`;
+// var logoname = `${tg.initDataUnsafe.user.username}`[0] + `${tg.initDataUnsafe.user.username}`[1];
+// var username = `${tg.initDataUnsafe.user.username}`;
 
-sessionStorage.setItem("userIdData", userIdData);
-sessionStorage.setItem("logoname", logoname);
-sessionStorage.setItem("username", username);
+// sessionStorage.setItem("userIdData", userIdData);
+// sessionStorage.setItem("logoname", logoname);
+// sessionStorage.setItem("username", username);
