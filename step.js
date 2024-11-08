3f496b8053df38da796156fd70ec2e086c374f30
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2024-11-08 12:37:30 +0300

    add function view images

--- step.js
+++ step.js
@@ -1,9 +1,13 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const syllabusId = urlParams.get("syllabusId");
-const moduleId = urlParams.get("moduleId");
-const submoduleId = urlParams.get("submoduleId");
-const stepId = urlParams.get("stepId");
+const moduleId = Number(urlParams.get("moduleId"));
+const submoduleId = Number(urlParams.get("submoduleId"));
+const stepId = Number(urlParams.get("stepId"));
+
+const buttonBack = document.getElementById("button-back");
+const buttonForward = document.getElementById("button-forward");
+const button = document.getElementById("button-next-step");
 
 const userId = localStorage.getItem("userIdData");
 
@@ -15,18 +19,21 @@ const mediaContent = document.getElementById("content");
 const modulesData = JSON.parse(
   localStorage.getItem(`courseData`)
 ).courseModuleList;
-// console.log(modulesData)
 const submoduleLength = modulesData[moduleId - 1].submoduleList;
 const stepInfo =
   modulesData[moduleId - 1].submoduleList[submoduleId - 1].stepList;
 const stepProgres = stepInfo[stepId - 1];
-console.log(stepProgres);
-// const stepIdProgress = stepInfo[stepId - 1].id;
 // const urlContent = stepInfo[stepId - 1].textContentUrl;
 var urlContent = stepInfo[stepId - 1].textContentUrl;
 urlContent =
   "https://raw.githubusercontent.com/AndreyErmol/CunaEduFiles/refs/heads/main/courses/technicalAnalysis/content.txt";
+// urlContent = "test.txt"
 const testContent = stepInfo[stepId - 1].test;
+// const testContent = {
+//   question: "Какой язык используется для создания веб-страниц?",
+//   options: ["HTML", "CSS", "JavaScript"],
+//   answer: "HTML",
+// };
 
 title.innerText = modulesData[moduleId - 1].submoduleList[submoduleId - 1].name;
 arrow.href = `syllabus.html?id=${syllabusId}`;
@@ -34,7 +41,7 @@ arrow.href = `syllabus.html?id=${syllabusId}`;
 steps.innerHTML = `${stepId} из ${stepInfo.length}`;
 
 var progress = JSON.parse(localStorage.getItem("completedSteps"));
-console.log(progress)
+
 function addStepProgress() {
   if (stepProgres.completed === false) {
     // Если массив пустой или шаг еще не добавлен
@@ -107,6 +114,7 @@ function displayTest() {
   submitButton.style.display = "flex";
   resultContainer.style.display = "flex";
   testDiv.innerHTML = `<p style="margin-bottom: 10px">${testContent.question}</p>`;
+
   testContent.options.forEach((option) => {
     const label = document.createElement("label");
     label.innerHTML = `
@@ -115,9 +123,11 @@ function displayTest() {
     `;
     testDiv.append(label);
   });
+
   resultContainer.innerText = "";
   retryButton.style.display = "none";
   submitButton.classList.add("disabled");
+
   const inputs = document.querySelectorAll('input[name="question"]');
   inputs.forEach((input) => {
     input.addEventListener("change", () => {
@@ -131,73 +141,12 @@ function displayTest() {
       `input[name="question"]:checked`
     );
     if (selectedOption) {
-      if (selectedOption.value === testContent.answer) {
-        submitButton.style.animation = "fadeOut 0.2s ease";
-        setTimeout(() => {
-          submitButton.style.display = "none";
-          submitButton.style.animation = "none";
-          nextButton.style.display = "flex";
-          nextButton.style.animation = "fadeIn 0.2s ease";
-          resultContainer.innerText = "Правильно!";
-          resultContainer.style.animation = "fadeIn 0.2s ease";
-          setTimeout(() => {
-            nextButton.style.animation = "none";
-            resultContainer.style.animation = "none";
-          }, 200);
-        }, 200);
-        addStepProgress();
-        if (
-          (stepId == Object.keys(stepInfo).length) &
-          (submoduleId != Object.keys(submoduleLength).length)
-        ) {
-          nextButton.href = `step.html?syllabusId=${syllabusId}&moduleId=${moduleId}&submoduleId=${
-            Number(submoduleId) + 1
-          }&stepId=${1}`;
-        } else if (
-          (submoduleId == Object.keys(submoduleLength).length) &
-          (moduleId != Object.keys(modulesData).length)
-        ) {
-          nextButton.href = `step.html?syllabusId=${syllabusId}&moduleId=${
-            Number(moduleId) + 1
-          }&submoduleId=${1}&stepId=${1}`;
-        } else if (
-          (moduleId == Object.keys(modulesData).length) &
-          (submoduleId == Object.keys(submoduleLength).length) &
-          (stepId == Object.keys(stepInfo).length)
-        ) {
-          nextButton.removeAttribute("href");
-          nextButton.addEventListener("click", function () {
-            alert("HUY");
-          });
-        } else {
-          nextButton.href = `step.html?syllabusId=${syllabusId}&moduleId=${moduleId}&submoduleId=${submoduleId}&stepId=${
-            Number(stepId) + 1
-          }`;
-        }
-      } else {
-        submitButton.style.animation = "fadeOut 0.2s ease";
-        setTimeout(() => {
-          submitButton.style.display = "none";
-          submitButton.style.animation = "none";
-          retryButton.style.display = "flex";
-          retryButton.style.animation = "fadeIn 0.2s ease";
-          resultContainer.innerText = "Неправильно!";
-          resultContainer.style.animation = "fadeIn 0.2s ease";
-          setTimeout(() => {
-            resultContainer.style.animation = "none";
-            retryButton.style.animation = "none";
-          }, 200);
-        }, 200);
-      }
-
-      const inputs = document.querySelectorAll('input[name="question"]');
-      inputs.forEach((input) => {
-        input.disabled = true;
-      });
+      handleAnswer(selectedOption.value);
     } else {
       submitButton.disabled = true;
     }
   });
+
   retryButton.addEventListener("click", () => {
     displayTest();
     submitButton.style.display = "flex";
@@ -209,6 +158,177 @@ function displayTest() {
     // resultContainer.innerText = `Вы набрали ${score} баллов.`;
   });
 }
+
+function handleAnswer(selectedValue) {
+  if (selectedValue === testContent.answer) {
+    handleCorrectAnswer();
+  } else {
+    handleIncorrectAnswer();
+  }
+}
+
+function handleCorrectAnswer() {
+  submitButton.style.animation = "fadeOut 0.2s ease";
+
+  setTimeout(() => {
+    submitButton.style.display = "none";
+    submitButton.style.animation = "none";
+    nextButton.style.display = "flex";
+    nextButton.style.animation = "fadeIn 0.2s ease";
+
+    resultContainer.innerText = "Правильно!";
+    resultContainer.style.animation = "fadeIn 0.2s ease";
+
+    setTimeout(() => {
+      nextButton.style.animation = "none";
+      resultContainer.style.animation = "none";
+    }, 200);
+  }, 200);
+  addStepProgress();
+
+  updateNextButtonHref();
+}
+
+function handleIncorrectAnswer() {
+  submitButton.style.animation = "fadeOut 0.2s ease";
+
+  setTimeout(() => {
+    submitButton.style.display = "none";
+    submitButton.style.animation = "none";
+
+    retryButton.style.display = "flex";
+    retryButton.style.animation = "fadeIn 0.2s ease";
+
+    resultContainer.innerText = "Неправильно!";
+    resultContainer.style.animation = "fadeIn 0.2s ease";
+
+    setTimeout(() => {
+      resultContainer.style.animation = "none";
+      retryButton.style.animation = "none";
+    }, 200);
+  }, 200);
+}
+
+const totalSteps = Object.keys(stepInfo).length;
+const totalSubmodules = Object.keys(submoduleLength).length;
+const totalModules = Object.keys(modulesData).length;
+
+function updateNextButtonHref() {
+  if (stepId == totalSteps && submoduleId != totalSubmodules) {
+    nextButton.href = `step.html?syllabusId=${syllabusId}&moduleId=${moduleId}&submoduleId=${
+      submoduleId + 1
+    }&stepId=1`;
+  } else if (submoduleId == totalSubmodules && moduleId != totalModules) {
+    nextButton.href = `step.html?syllabusId=${syllabusId}&moduleId=${
+      moduleId + 1
+    }&submoduleId=1&stepId=1`;
+  } else if (
+    moduleId == totalModules &&
+    submoduleId == totalSubmodules &&
+    stepId == totalSteps
+  ) {
+    nextButton.removeAttribute("href");
+    nextButton.addEventListener("click", function () {
+      alert("HUY");
+    });
+  } else {
+    nextButton.href = `step.html?syllabusId=${syllabusId}&moduleId=${moduleId}&submoduleId=${submoduleId}&stepId=${
+      stepId + 1
+    }`;
+  }
+}
+
+function setButtonHref(button, href) {
+  if (href) button.href = href;
+  else button.removeAttribute("href");
+}
+
+// Обработка кнопки "Назад"
+setButtonHref(
+  buttonBack,
+  stepId == 1
+    ? null
+    : `step.html?syllabusId=${syllabusId}&moduleId=${moduleId}&submoduleId=${submoduleId}&stepId=${
+        stepId - 1
+      }`
+);
+
+// Обработка кнопки "Вперед"
+setButtonHref(
+  buttonForward,
+  stepId == totalSteps
+    ? null
+    : `step.html?syllabusId=${syllabusId}&moduleId=${moduleId}&submoduleId=${submoduleId}&stepId=${
+        stepId + 1
+      }`
+);
+
+// Обработка кнопки "Следующий шаг"
+if (stepId == totalSteps) {
+  if (submoduleId < totalSubmodules) {
+    setButtonHref(
+      button,
+      `step.html?syllabusId=${syllabusId}&moduleId=${moduleId}&submoduleId=${
+        submoduleId + 1
+      }&stepId=1`
+    );
+  } else if (moduleId < totalModules) {
+    setButtonHref(
+      button,
+      `step.html?syllabusId=${syllabusId}&moduleId=${
+        moduleId + 1
+      }&submoduleId=1&stepId=1`
+    );
+  } else {
+    setButtonHref(button, null);
+    button.addEventListener("click", function () {
+      alert("HUY");
+    });
+  }
+} else {
+  setButtonHref(
+    button,
+    `step.html?syllabusId=${syllabusId}&moduleId=${moduleId}&submoduleId=${submoduleId}&stepId=${
+      stepId + 1
+    }`
+  );
+}
+
+const refer = localStorage.getItem("refer");
+const favorTab = document.getElementById("favor");
+const catalogTab = document.getElementById("catalog");
+var link = document.referrer.split("/").pop();
+link = link.split("&").pop();
+const switc = document.getElementById("switc");
+
+sessionStorage.setItem("currentTab", refer);
+sessionStorage.setItem("currentLink", window.location.href);
+
+function setupTab(tab) {
+  tab.style.animation = "none";
+  tab.style.color = "#ffffff";
+  tab.addEventListener("click", function () {
+    sessionStorage.removeItem("currentTab");
+    sessionStorage.removeItem("currentLink");
+  });
+}
+
+if (refer == "index.html" || refer == "favorite.html") {
+  setupTab(favorTab);
+} else if (refer == "catalog.html") {
+  setupTab(catalogTab);
+}
+
+if (stepId != 1 || link == "stepId=2") {
+  title.style.animation = "none";
+  switc.style.animation = "none";
+}
+
+window.addEventListener("beforeunload", function () {
+  if (link != `stepId=${stepId}` && progress.completedStepList.length !== 0) {
+    sendProgress();
+  }
+});
 
 // function getLocalStorageSize() {
 //   let total = 0;
@@ -236,97 +356,3 @@ function displayTest() {
 
 // const key = "coursesData"; // Замените на ваш ключ
 // console.log(`Размер элемента '${key}': ${getItemSize(key)} KB`);
-
-const buttonBack = document.getElementById("button-back");
-if (stepId == 1) {
-  buttonBack.removeAttribute("href");
-} else {
-  buttonBack.href = `step.html?syllabusId=${syllabusId}&moduleId=${moduleId}&submoduleId=${submoduleId}&stepId=${
-    Number(stepId) - 1
-  }`;
-}
-
-const buttonForward = document.getElementById("button-forward");
-if (stepId == Object.keys(stepInfo).length) {
-  buttonForward.removeAttribute("href");
-} else {
-  buttonForward.href = `step.html?syllabusId=${syllabusId}&moduleId=${moduleId}&submoduleId=${submoduleId}&stepId=${
-    Number(stepId) + 1
-  }`;
-}
-
-const button = document.getElementById("button-next-step");
-
-if (
-  stepId == Object.keys(stepInfo).length &&
-  submoduleId != Object.keys(submoduleLength).length
-) {
-  console.log("1");
-  button.href = `step.html?syllabusId=${syllabusId}&moduleId=${moduleId}&submoduleId=${
-    Number(submoduleId) + 1
-  }&stepId=${1}`;
-} else if (
-  stepId == Object.keys(stepInfo).length &&
-  submoduleId == Object.keys(submoduleLength).length &&
-  moduleId != Object.keys(modulesData).length
-) {
-  console.log("2");
-
-  button.href = `step.html?syllabusId=${syllabusId}&moduleId=${
-    Number(moduleId) + 1
-  }&submoduleId=${1}&stepId=${1}`;
-} else if (
-  stepId == Object.keys(stepInfo).length &&
-  moduleId == Object.keys(modulesData).length &&
-  submoduleId == Object.keys(submoduleLength).length
-) {
-  console.log("3");
-
-  button.removeAttribute("href");
-  button.addEventListener("click", function () {
-    alert("HUY");
-  });
-} else if (stepId != Object.keys(stepInfo).length) {
-  console.log("4");
-
-  button.href = `step.html?syllabusId=${syllabusId}&moduleId=${moduleId}&submoduleId=${submoduleId}&stepId=${
-    Number(stepId) + 1
-  }`;
-}
-
-const refer = localStorage.getItem("refer");
-const favorTab = document.getElementById("favor");
-const catalogTab = document.getElementById("catalog");
-var link = document.referrer.split("/").pop();
-link = link.split("&").pop();
-const switc = document.getElementById("switc");
-
-sessionStorage.setItem("currentTab", refer);
-sessionStorage.setItem("currentLink", window.location.href);
-
-if (refer == "index.html" || refer == "favorite.html") {
-  favorTab.style.animation = "none";
-  favorTab.style.color = "#ffffff";
-  favorTab.addEventListener("click", function () {
-    sessionStorage.removeItem("currentTab");
-    sessionStorage.removeItem("currentLink");
-  });
-} else if (refer == "catalog.html") {
-  catalogTab.style.animation = "none";
-  catalogTab.style.color = "#ffffff";
-  catalogTab.addEventListener("click", function () {
-    sessionStorage.removeItem("currentTab");
-    sessionStorage.removeItem("currentLink");
-  });
-}
-
-if (stepId != 1 || link == "stepId=2") {
-  title.style.animation = "none";
-  switc.style.animation = "none";
-}
-
-window.addEventListener("beforeunload", function () {
-  if (link != `stepId=${stepId}` && progress.completedStepList.length !== 0) {
-    sendProgress();
-  }
-});
