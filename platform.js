5fdb4c71d823635c0e22a58412eb5c78af0baea4
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2024-11-28 13:39:11 +0300

    debug

--- platform.js
+++ platform.js
@@ -11,6 +11,7 @@ if (platform === "ios" || platform === "android") {
 tg.lockOrientation();
 tg.expand();
 tg.enableClosingConfirmation();
+tg.enableVerticalSwipes()
 
 document.getElementById("tab").addEventListener("click", function () {
   tg.HapticFeedback.impactOccurred("medium");
