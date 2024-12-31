e0d7308fc00f13bf409647f22cb61d02ad38ff71
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2024-12-31 20:53:58 +0300

    debug

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 localStorage.clear();
 
-let initDataURLSP = new URLSearchParams(tg.initData);
-const url = tg.initDataURLSP.get('photo_url')
-alert(url)
-
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const referallId = urlParams.get("tgWebAppStartParam");
 
 localStorage.setItem("referallId", referallId);
 
-// window.location.href = "favorite.html"
+window.location.href = "favorite.html"
