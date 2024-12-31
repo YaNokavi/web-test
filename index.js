ac0bdc39f5506cef2fb0a8aab5b747ecc11a3667
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2024-12-31 20:44:47 +0300

    debug

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ localStorage.clear();
 
 let initDataURLSP = new URLSearchParams(tg.initData);
 const url = tg.initDataURLSP.get('photo_url')
+alert(url)
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
