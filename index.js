c2d35fef5fbea6090b1fa99a705c4052a1f12896
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2024-12-31 20:29:54 +0300

    debug

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 localStorage.clear();
 
+alert(tg.initDataUnsafe.user.is_bot)
+
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const referallId = urlParams.get("tgWebAppStartParam");
 
 localStorage.setItem("referallId", referallId);
 
-window.location.href = "favorite.html"
+// window.location.href = "favorite.html"
