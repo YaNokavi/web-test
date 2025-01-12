1f044a8c864d6c330c27f2967f53b65a206088fc
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2025-01-12 03:29:14 +0300

    debug

--- profile.js
+++ profile.js
@@ -14,7 +14,6 @@ const userNameProfile = document.querySelector(".profile-nickname");
 const course = document.getElementById("course-info");
 
 const userIdData = tg.initDataUnsafe.user.id;
-
 let logoName;
 let userName;
 
