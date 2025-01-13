ecd8076b0d80f0275c8fc218401007cea7292c34
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2025-01-13 14:41:19 +0300

    debug

--- friends.js
+++ friends.js
@@ -189,14 +189,14 @@ function buttons() {
   const buttonInvite = document.getElementById("invite");
 
   buttonInvite.addEventListener("click", function () {
-    let a = `https://t.me/CunaEduBot?startapp=${userId}`,
+    let a = `https://t.me/CunaEduBot/CunaEdu?startapp=${userId}`,
       s = encodeURI(a),
       o = encodeURI("Узнавай новое вместе со мной (@cryptuna)");
     window.location.href = `https://t.me/share/url?url=${s}&text=${o}`;
   });
 
   document.getElementById("copy").addEventListener("click", function () {
-    let link = `https://t.me/cunaedu_bot?startapp=${userId}`;
+    let link = `https://t.me/cunaedu_bot/CunaEdu?startapp=${userId}`;
     navigator.clipboard.writeText(link);
 
     var svg1 = document.getElementById("svg1");
