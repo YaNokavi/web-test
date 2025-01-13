215f68ccd10a445debd2cae34e311eaa58175289
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2025-01-13 14:48:59 +0300

    debug

--- index.js
+++ index.js
@@ -6,57 +6,8 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const referallId = urlParams.get("tgWebAppStartParam");
 localStorage.setItem("referallId", referallId);
-const tg = window.Telegram.WebApp;
-// alert(tg.initDataUnsafe.user);
-
-// ?tgWebAppStartParam=535799793
-
-if (!referallId) { 
-  alert("Вы зашли");
-} else {
-  alert("Пост");
-  sendUserBot();
-}
-
-async function sendUserBot() {
-  const data = {
-    message: {
-      chat: {
-        id: tg.initDataUnsafe.user.id,
-        first_name: tg.initDataUnsafe.user.first_name,
-        username: tg.initDataUnsafe.user.username,
-      },
-      text: `https://t.me/CunaEduBot?startapp=${tg.initDataUnsafe.user.id}`,
-      from: {
-        id: tg.initDataUnsafe.user.id,
-        first_name: tg.initDataUnsafe.user.first_name,
-        username: tg.initDataUnsafe.user.username,
-      },
-    },
-
-    // message: {
-    //   chat: {
-    //     id: 535799793,
-    //     first_name: "Test",
-    //     username: "test_user",
-    //   },
-    //   text: "https://t.me/CunaEduBot?startapp=535799793",
-    //   from: {
-    //     id: 123456789,
-    //     first_name: "Test",
-    //     username: "test_user",
-    //   },
-    // },
-  };
-  const response = await fetchData(
-    `https://cunaedubot-test-anderm.amvera.io/webhook`,
-    "POST",
-    data
-  );
-  alert(response);
-}
 
 const flagFirstJoin = true;
 localStorage.setItem("flagFirstJoin", flagFirstJoin);
 
-// window.location.href = "favorite.html";
+window.location.href = "favorite.html";
