d2b2b967147a7d278499fd7c1dc4ce17ac5eff7f
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2025-01-13 11:15:41 +0300

    test tg.initData

--- index.js
+++ index.js
@@ -7,20 +7,27 @@ const urlParams = new URLSearchParams(queryString);
 const referallId = urlParams.get("tgWebAppStartParam");
 localStorage.setItem("referallId", referallId);
 const tg = window.Telegram.WebApp;
-alert(tg.initDataUnsafe.user)
+// alert(tg.initDataUnsafe.user);
 
-// if (!referallId || referallId === userIdData) {
-
-// }
-// else {
-//   data = {}
-//   await fetchData(
-//     `https://cunaedubot-test-anderm.amvera.io/webhook`,
-//     "POST",
-//     data,
-//     false
-//   );
-// }
+if (!referallId || referallId === userIdData) {
+  alert("Вы зашли");
+} else {
+  data = {
+    chat: {
+      id: tg.initDataUnsafe.user.id,
+    },
+    text: `https://t.me/CunaEduBot?startapp=${referallId}`,
+    from: {
+      id: referallId,
+    },
+  };
+  await fetchData(
+    `https://cunaedubot-test-anderm.amvera.io/webhook`,
+    "POST",
+    data,
+    false
+  );
+}
 
 const flagFirstJoin = true;
 localStorage.setItem("flagFirstJoin", flagFirstJoin);
