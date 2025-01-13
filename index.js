7cfb424db9deae6d3fc14ad37b455ca1d2d9edf4
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2025-01-13 10:50:40 +0300

    test tg.initData

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+import fetchData from "./fetch";
+
 localStorage.clear();
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const referallId = urlParams.get("tgWebAppStartParam");
 localStorage.setItem("referallId", referallId);
+alert(JSON.parse(tg.initData))
+
+// if (!referallId || referallId === userIdData) {
+
+// }
+// else {
+//   data = {}
+//   await fetchData(
+//     `https://cunaedubot-test-anderm.amvera.io/webhook`,
+//     "POST",
+//     data,
+//     false
+//   );
+// }
 
 const flagFirstJoin = true;
 localStorage.setItem("flagFirstJoin", flagFirstJoin);
