d3414a4d2daebe5c9fc25bfc3607691afff73cc5
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2025-01-13 11:00:06 +0300

    test tg.initData

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const urlParams = new URLSearchParams(queryString);
 const referallId = urlParams.get("tgWebAppStartParam");
 localStorage.setItem("referallId", referallId);
 const tg = window.Telegram.WebApp;
-alert(JSON.parse(tg.initData))
+alert(tg.initData)
 
 // if (!referallId || referallId === userIdData) {
 
