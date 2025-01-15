b2b0bc999ddeca3790fa2e58613f124b540e3301
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2025-01-15 03:45:39 +0300

    debug

--- platform.js
+++ platform.js
@@ -12,51 +12,107 @@ const version = Number(tg.version);
 //   window.location.href = "nontg.html";
 // }
 
-tg.onEvent('themeChanged', function() {
+tg.onEvent("themeChanged", function () {
   const theme = tg.colorScheme;
-
-    if (theme === 'light') {
-      document.documentElement.style.setProperty("--theme-bg-color", `#efeff4`);
-      document.documentElement.style.setProperty("--theme-bg-hint-color", `rgba(20, 104, 177, 0.15)`);
-      document.documentElement.style.setProperty("--theme-header-tab-color", `#1468b1`);
-      document.documentElement.style.setProperty("--theme-tab-icon-text-color", `#9bb1c5`);
-      document.documentElement.style.setProperty("--theme-block-color", `#ffffff`);
-      document.documentElement.style.setProperty("--theme-block-border-color", `rgba(0, 0, 0, 0.1)`);
-      document.documentElement.style.setProperty("--theme-text-color", `#ffffff`);
-      document.documentElement.style.setProperty("--theme-text-hint-color", `rgba(0, 0, 0, 0.41)`);
-      document.documentElement.style.setProperty("--theme-icon-color", `#ffffff`);
-      document.documentElement.style.setProperty("--theme-button-color", `#1468b1`);
-      document.documentElement.style.setProperty("--theme-button-icon-text-color", `#ffffff`);
-      document.documentElement.style.setProperty("--theme-button-hint-color", `rgba(20, 104, 177, 0.15)`);
-      document.documentElement.style.setProperty("--theme-button-hint-icon-text-color", `#1468b1`);
-    } else {
-      document.documentElement.style.setProperty("--theme-bg-color", `#131313`);
-      document.documentElement.style.setProperty("--theme-bg-hint-color", `rgba(203, 202, 198, 0.1)`);
-      document.documentElement.style.setProperty("--theme-header-tab-color", `#191919`);
-      document.documentElement.style.setProperty("--theme-tab-icon-text-color", `#707070`);
-      document.documentElement.style.setProperty("--theme-block-color", `#1A1A1A`);
-      document.documentElement.style.setProperty("--theme-block-border-color", `rgba(255, 255, 255, 0.1)`);
-      document.documentElement.style.setProperty("--theme-text-color", `#CBCAC6`);
-      document.documentElement.style.setProperty("--theme-text-hint-color", `9E9E9E`);
-      document.documentElement.style.setProperty("--theme-icon-color", `#CBCAC6`);
-      document.documentElement.style.setProperty("--theme-button-color", `#CBCAC6`);
-      document.documentElement.style.setProperty("--theme-button-icon-text-color", `#000000`);
-      document.documentElement.style.setProperty("--theme-button-hint-color", `rgba(203, 202, 198, 0.1)`);
-      document.documentElement.style.setProperty("--theme-button-hint-icon-text-color", `#CBCAC6`);
-        // document.body.classList.add('dark-theme');
-        // document.body.classList.remove('light-theme');
-    }
+  if (theme === "light") {
+    document.documentElement.style.setProperty("--theme-bg-color", `#efeff4`);
+    document.documentElement.style.setProperty(
+      "--theme-bg-hint-color",
+      `rgba(20, 104, 177, 0.15)`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-header-tab-color",
+      `#1468b1`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-tab-icon-text-color",
+      `#9bb1c5`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-block-color",
+      `#ffffff`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-block-border-color",
+      `rgba(0, 0, 0, 0.1)`
+    );
+    document.documentElement.style.setProperty("--theme-text-color", `#ffffff`);
+    document.documentElement.style.setProperty(
+      "--theme-text-hint-color",
+      `#a6a6a6`
+    );
+    document.documentElement.style.setProperty("--theme-icon-color", `#ffffff`);
+    document.documentElement.style.setProperty(
+      "--theme-button-color",
+      `#1468b1`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-button-icon-text-color",
+      `#ffffff`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-button-hint-color",
+      `rgba(20, 104, 177, 0.15)`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-button-hint-icon-text-color",
+      `#1468b1`
+    );
+  } else {
+    document.documentElement.style.setProperty("--theme-bg-color", `#131313`);
+    document.documentElement.style.setProperty(
+      "--theme-bg-hint-color",
+      `rgba(203, 202, 198, 0.1)`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-header-tab-color",
+      `#191919`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-tab-icon-text-color",
+      `#707070`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-block-color",
+      `#1A1A1A`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-block-border-color",
+      `rgba(255, 255, 255, 0.1)`
+    );
+    document.documentElement.style.setProperty("--theme-text-color", `#CBCAC6`);
+    document.documentElement.style.setProperty(
+      "--theme-text-hint-color",
+      `9E9E9E`
+    );
+    document.documentElement.style.setProperty("--theme-icon-color", `#CBCAC6`);
+    document.documentElement.style.setProperty(
+      "--theme-button-color",
+      `#CBCAC6`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-button-icon-text-color",
+      `#000000`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-button-hint-color",
+      `rgba(203, 202, 198, 0.1)`
+    );
+    document.documentElement.style.setProperty(
+      "--theme-button-hint-icon-text-color",
+      `#CBCAC6`
+    );
+    // document.body.classList.add('dark-theme');
+    // document.body.classList.remove('light-theme');
+  }
 });
 
-const initialTheme = tg.colorScheme;
-if (initialTheme === 'light') {
-  alert(`Светлая`);
-    // document.body.classList.add('light-theme');
-} else {
-  alert(`темная`);
-    // document.body.classList.add('dark-theme');
-}
-
+// const initialTheme = tg.colorScheme;
+// if (initialTheme === "light") {
+//   alert(`Светлая`);
+// } else {
+//   alert(`темная`);
+// }
 
 tg.setHeaderColor("#1468B1");
 
